b33d8cec5617f2cf81445f97396351b1e91666e9
WonderDeep <sfaust3@masonlive.gmu.edu>
2015-03-31 23:27:19 -0400

    The core
    The game's spine is set!

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-var game = new Phaser.Game(600, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(900, 500, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
-
+	game.load.image('bg', 'assets/cpu.jpg');
+	game.load.image('n0', 'assets/n0.jpg');
+	game.load.image('n1', 'assets/n1.jpg');
+	game.load.image('n2', 'assets/n2.jpg');
+	game.load.image('n3','assets/n3.jpg');
+	game.load.image('n4','assets/n4.jpg');
+	game.load.image('n5','assets/n5.jpg');
+	game.load.image('n6','assets/n6.jpg');
+	game.load.image('newline','assets/newline.jpg');
+	game.load.image('intro1', 'assets/intro1.jpg');
+	game.load.image('intro2','assets/intro2.jpg');
+	game.load.image('intro3','assets/intro3.jpg');
+	game.load.image('ok','assets/ok.jpg');
+	game.load.image('next','assets/next.jpg');
 }
 
 var num1;
@@ -12,7 +25,12 @@ var guess1;
 var guess2;
 var guess3;
 var guess4;
-var guessCount= 0;
+var guessCount= 0; //what line are we on?
+var numCount = 0; //what digit are we on?
+var background;
+var next;
+
+
 
 function create() {
 	var num1 = game.rnd.integerInRange(1, 6);
@@ -20,20 +38,202 @@ function create() {
 	var num3 = game.rnd.integerInRange(1, 6);
 	var num4 = game.rnd.integerInRange(1, 6);
 	
-	this.wasd = {
-                g1: game.input.keyboard.addKey(Phaser.Keyboard.1),
-                g2: game.input.keyboard.addKey(Phaser.Keyboard.2),
-                g3: game.input.keyboard.addKey(Phaser.Keyboard.3),
-                g4: game.input.keyboard.addKey(Phaser.Keyboard.4),
-                g5: game.input.keyboard.addKey(Phaser.Keyboard.5),
-                g6: game.input.keyboard.addKey(Phaser.Keyboard.6),
+	background = game.add.sprite(0, 0, 'intro1');
+	next = game.add.sprite(440,300, 'next');
+	
+	next.inputEnabled = true;
+  	next.events.onInputDown.add(infopage1, this);
+	
+	this.nums = {
+                g1: game.input.keyboard.addKey(Phaser.Keyboard.Q),
+                g2: game.input.keyboard.addKey(Phaser.Keyboard.W),
+                g3: game.input.keyboard.addKey(Phaser.Keyboard.E),
+                g4: game.input.keyboard.addKey(Phaser.Keyboard.R),
+                g5: game.input.keyboard.addKey(Phaser.Keyboard.T),
+                g6: game.input.keyboard.addKey(Phaser.Keyboard.Y),
             };
 	
 }
 
+function infopage1 () {
+	background.loadTexture('intro2',0);
+	next.inputEnabled = true;
+  	next.events.onInputDown.add(infopage2, this);
+}
+
+function infopage1 () {
+	background.loadTexture('intro3',0);
+	next.loadTexture('ok',0);
+	next.inputEnabled = true;
+  	next.events.onInputDown.add(update, this);
+}
+
 function update() {
+	if(this.nums.g1.onTap)
+	{
+	/*
+		make1()
+		
+		 
+		 
+		 if(numCount===4){
+		 	(call checking function that prints results 
+		 		@ x=450, y=guessCount+3)
+		 	numCount = 0;
+		 	guessCount += 50;
+		 }
+		 
+		 
+		 if(guessCount===100)
+		 {
+		 	(call endGame)
+		 }
+	*/
+			
+	}
+	
+	if(this.nums.g2.onTap)
+	{
+		make2();
+	}
+	
+	if(this.nums.g3.onTap)
+	{
+		make3();
+	}
+	
+	if(this.nums.g4.onTap)
+	{
+		make4();
+	}
+	
+	if(this.nums.g5.onTap)
+	{
+		make5();
+	}
+	
+	if(this.nums.g6.onTap)
+	{
+		make6();
+	}
 
+}
 
+function make1 () {
+	var xval = (numCount*20)+5;
+	numCount += 1;
+	var yval = guessCount + 3;
+	var someSprite = game.add.sprite(xval,yval, 'n1');
+
+	
+	if(numCount===1){
+		guess1 = 1;
+	}
+	if(numCount===2){
+		guess2 = 1;
+	}
+	if(numCount===3){
+		guess3 = 1;
+	}
+	if(numCount===4){
+		guess4 = 1;
+	}
+	
+}
+function make2 () {
+	var xval = (numCount*20)+5;
+	numCount += 1;
+	var yval = guessCount + 3;
+	var someSprite = game.add.sprite(xval,yval, 'n2');
+	
+	if(numCount===1){
+		guess1 = 2;
+	}
+	if(numCount===2){
+		guess2 = 2;
+	}
+	if(numCount===3){
+		guess3 = 2;
+	}
+	if(numCount===4){
+		guess4 = 2;
+	}
+}
+function make3 () {
+	var xval = (numCount*20)+5;
+	numCount += 1;
+	var yval = guessCount + 3;
+	var someSprite = game.add.sprite(xval,yval, 'n3');
+	
+	if(numCount===1){
+		guess1 = 3;
+	}
+	if(numCount===2){
+		guess2 = 3;
+	}
+	if(numCount===3){
+		guess3 = 3;
+	}
+	if(numCount===4){
+		guess4 = 3;
+	}
+}
+function make4 () {
+	var xval = (numCount*20)+5;
+	numCount += 1;
+	var yval = guessCount + 3;
+	var someSprite = game.add.sprite(xval,yval, 'n4');
+	
+	if(numCount===1){
+		guess1 = 4;
+	}
+	if(numCount===2){
+		guess2 = 4;
+	}
+	if(numCount===3){
+		guess3 = 4;
+	}
+	if(numCount===4){
+		guess4 = 4;
+	}
+}
+function make5 () {
+	var xval = (numCount*20)+5;
+	numCount += 1;
+	var yval = guessCount + 3;
+	var someSprite = game.add.sprite(xval,yval, 'n5');
+	
+	if(numCount===1){
+		guess1 = 5;
+	}
+	if(numCount===2){
+		guess2 = 5;
+	}
+	if(numCount===3){
+		guess3 = 5;
+	}
+	if(numCount===4){
+		guess4 = 5;
+	}
+}
+function make6 () {
+	var xval = (numCount*20)+5;
+	numCount += 1;
+	var yval = guessCount + 3;
+	var someSprite = game.add.sprite(xval,yval, 'n6');
+	
+	if(numCount===1){
+		guess1 = 6;
+	}
+	if(numCount===2){
+		guess2 = 6;
+	}
+	if(numCount===3){
+		guess3 = 6;
+	}
+	if(numCount===4){
+		guess4 = 6;
+	}
 }
 
 function render () {
